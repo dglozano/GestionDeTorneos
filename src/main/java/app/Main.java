8d9f3861f0e331d4ccdb63b27f6ab5ac306b4599
Juan Ferreras <juan@flioh.com>
2015-10-24 19:25:40 -0300

    Vistas usan CSS global. Cambios en la vista principal.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -4,16 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+        Font.loadFont(getClass().getResource("../fonts/OpenSans-Regular.ttf").toExternalForm(), 10);
+
         Parent root = FXMLLoader.load(getClass().getResource("../fmxl/main.fxml"));
+
         primaryStage.setTitle("Gestion De Torneos");
-        primaryStage.setScene(new Scene(root,900,600));
-        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 1000, 600));
+        //primaryStage.setResizable(false);
 
         primaryStage.show();
     }
--- src/main/java/models/Disponibilidad.java
+++ src/main/java/models/Disponibilidad.java
@@ -9,6 +9,11 @@ import javax.persistence.*;
 @Entity
 public class Disponibilidad {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
     @Column(name = "disponibilidad")
     private Integer disponibilidad;
     @ManyToOne
