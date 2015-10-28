2f60c7c5c85cd7b5ed329142a795bb47c929f42f
Juan Ferreras <juan@flioh.com>
2015-10-28 08:45:47 -0300

    Cambio de preloader a main staticamente a los 3.5 seg

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,26 +1,66 @@
 package app;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Main extends Application{
+
+    private Stage stage;
+    private static Main instance;
+
+    public Main(){
+        instance = this;
+    }
+
+    public static Main getInstance(){
+        return instance;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Font.loadFont(getClass().getClassLoader().getResource("fonts/OpenSans-Italic.ttf").toExternalForm(), 10);
         Font.loadFont(getClass().getClassLoader().getResource("fonts/OpenSans-Regular.ttf").toExternalForm(), 10);
 
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fmxl/main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fmxl/preloader.fxml"));
+
+        stage = primaryStage;
 
         primaryStage.setTitle("Gestion De Torneos");
         primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.setResizable(false);
 
         primaryStage.show();
+
+        Timeline timeline = new Timeline(new KeyFrame(
+                Duration.millis(3500),
+                ae -> {
+                    try {
+                        replaceSceneContent("main.fxml");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }));
+        timeline.play();
+    }
+
+    private Parent replaceSceneContent(String fxml) throws Exception {
+        Parent page = FXMLLoader.load(getClass().getClassLoader().getResource("fmxl/" + fxml));
+        Scene scene = stage.getScene();
+        if (scene == null) {
+            scene = new Scene(page, 1000, 600);
+            stage.setScene(scene);
+        } else {
+            stage.getScene().setRoot(page);
+        }
+        stage.sizeToScene();
+        return page;
     }
 
     public static void main(String[] args) {
