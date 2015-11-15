c96eb092db610d6e7c186024417e475fa3c97d4a
Juan Ferreras <juan@flioh.com>
2015-11-15 11:16:55 -0300

    Fechas se generan dinámicamente en la vista mostrarFixture

--- src/main/java/controllers/mostrarFixtureController.java
+++ src/main/java/controllers/mostrarFixtureController.java
@@ -6,7 +6,10 @@ import controllers.general.PrincipalController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import models.Competencia;
@@ -25,6 +28,7 @@ public class mostrarFixtureController implements ControlledScreen {
     private Parent parent;
 
     @FXML private Text title;
+    @FXML private TabPane fechas;
 
     @Override
     public void setScreenParent(PrincipalController screenParent){
@@ -36,6 +40,8 @@ public class mostrarFixtureController implements ControlledScreen {
         idCompetencia = (Integer) myController.getControladorAnterior().mensajeControladorAnterior();
         competencia = gestorCompetencia.buscarCompetenciaPorId(idCompetencia);
         title.setText(competencia.getNombre());
+
+        generarTabs();
     }
 
     @Override
@@ -50,5 +56,31 @@ public class mostrarFixtureController implements ControlledScreen {
 
     public void volver(ActionEvent actionEvent){
         myController.setScreen(Main.vistaVerCompetenciaId);
+    }
+
+    public void generarTabs(){
+
+        // genero 5 fechas
+        // TODO: vincular con cantidad de fechas de fixture
+        for(int i = 0; i<5; i++){
+            Tab tab = new Tab();
+            tab.setText("Fecha " + i);
+
+            TableView tabla = new TableView();
+            tabla.setPrefHeight(315.0);
+            tabla.setPrefWidth(779.0);
+            tabla.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+            TableColumn localColumn = new TableColumn("Participante"+i);
+            TableColumn resultadoColumn = new TableColumn("Resultado"+i);
+            TableColumn visitanteColumn = new TableColumn("Participante"+i);
+            TableColumn accionesColumn = new TableColumn("Acciones"+i);
+            tabla.getColumns().addAll(localColumn, resultadoColumn, visitanteColumn, accionesColumn);
+
+            // TODO: generar y llenar la tabla con los datos de la fecha
+
+            tab.setContent(tabla);
+            fechas.getTabs().add(tab);
+        }
     }
 }
